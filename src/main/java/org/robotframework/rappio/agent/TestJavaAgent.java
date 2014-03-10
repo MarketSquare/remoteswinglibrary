abedadf391f69675e659d86a0674109074fe5d65
Johannes Kuusela <jokuusel@Johanness-MacBook-Air.local>
2014-03-10 12:42:03 +0200

    First remote server connection

--- src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
@@ -2,10 +2,22 @@ package org.robotframework.rappio.agent;
 
 import java.lang.instrument.Instrumentation;
 
+import org.robotframework.remoteserver.RemoteServer;
+
 public class TestJavaAgent {
 
 	  public static void premain(String agentArgument, Instrumentation instrumentation){
 		  System.out.println("Java Agent!");
+		  RemoteServer.configureLogging();
+		  RemoteServer server = new RemoteServer();
+		  server.setPort(8181);
+		  try {
+			server.start();
+		} catch (Exception e) {
+			System.out.println("TODODOODODODODOODODODODO!!!");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	  }
 	  
 }
