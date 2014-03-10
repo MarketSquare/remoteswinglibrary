0adf3f3d06fd7b366228db0d23fa2b732f9f0ea3
Johannes Kuusela <jokuusel@Johanness-MacBook-Air.local>
2014-03-10 12:52:08 +0200

    Add SwingLibrary for testing the app

--- src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
@@ -3,6 +3,7 @@ package org.robotframework.rappio.agent;
 import java.lang.instrument.Instrumentation;
 
 import org.robotframework.remoteserver.RemoteServer;
+import org.robotframework.swing.SwingLibrary;
 
 public class TestJavaAgent {
 
@@ -10,6 +11,7 @@ public class TestJavaAgent {
 		  System.out.println("Java Agent!");
 		  RemoteServer.configureLogging();
 		  RemoteServer server = new RemoteServer();
+		  server.putLibrary("/", new SwingLibrary());
 		  server.setPort(8181);
 		  try {
 			server.start();
