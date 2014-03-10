4037036ff5e54c5d97434e80642cde003c7f64a5
Mikko Korpela <mikko.korpela@gmail.com>
2014-03-10 11:33:10 +0200

    Initial commit

---
+++ src/main/java/org/robotframework/rappio/App.java
@@ -0,0 +1,13 @@
+package org.robotframework.rappio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
