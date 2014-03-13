4ba05d09fb51388c5ceb55c911cb5591e249df32
Mikko Korpela <mikko.korpela@gmail.com>
2014-03-13 16:53:44 +0200

    still works on windows

--- src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
@@ -1,6 +1,6 @@
 package org.robotframework.rappio.agent;
 
-import java.awt.Frame;
+import java.awt.Window;
 import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
 
@@ -10,20 +10,18 @@ import org.robotframework.swing.SwingLibrary;
 public class TestJavaAgent {
 
 	public static void premain(String agentArgument, Instrumentation instrumentation){
-		System.out.println("Java Agent! "+ManagementFactory.getRuntimeMXBean().getVmVersion());
-		RemoteServer.configureLogging();
-		RemoteServer server = new RemoteServer();
-		server.putLibrary("/RPC2", new SwingLibrary());
-		server.setPort(8181);
-		server.setAllowStop(true);
-		try {
-			server.start();
-		} catch (Exception e) {
-			System.out.println("TODODOODODODODOODODODODO!!!");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-                Frame.getFrames();
+                System.out.println("Java Agent! "+ManagementFactory.getRuntimeMXBean().getVmVersion());
+                RemoteServer.configureLogging();
+                RemoteServer server = new RemoteServer();
+                server.putLibrary("/RPC2", new SwingLibrary());
+                server.setPort(8181);
+                server.setAllowStop(true);
+                try {
+                    server.start();
+                } catch (Exception e) {
+                    System.out.println("TODODOODODODODOODODODODO!!!");
+                }
+                Window.getWindows();
 	}
 
 }
