cc8593b29fc24270329df6db26bc8e9744e651f5
Johannes Kuusela <jokuusel@Johanness-MacBook-Air.local>
2014-03-13 13:51:07 +0200

    Added s.o.p lines in a scheduled executor

--- src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
@@ -1,26 +1,49 @@
 package org.robotframework.rappio.agent;
 
+import java.awt.Frame;
 import java.lang.instrument.Instrumentation;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.robotframework.remoteserver.RemoteServer;
 import org.robotframework.swing.SwingLibrary;
 
 public class TestJavaAgent {
+	static ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
-	  public static void premain(String agentArgument, Instrumentation instrumentation){
-		  System.out.println("Java Agent! ");
-		  RemoteServer.configureLogging();
-		  RemoteServer server = new RemoteServer();
-		  server.putLibrary("/RPC2", new SwingLibrary());
-		  server.setPort(8181);
-		  server.setAllowStop(true);
-		  try {
+	public static void premain(String agentArgument, Instrumentation instrumentation){
+		System.out.println("Java Agent! "+ManagementFactory.getRuntimeMXBean().getVmVersion());
+		RemoteServer.configureLogging();
+		RemoteServer server = new RemoteServer();
+		server.putLibrary("/RPC2", new SwingLibrary());
+		server.setPort(8181);
+		server.setAllowStop(true);
+		final Instrumentation inst = instrumentation;
+		try {
 			server.start();
 		} catch (Exception e) {
 			System.out.println("TODODOODODODODOODODODODO!!!");
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	  }
-	  
+		
+		Runnable helloRunnable = new Runnable() {
+			public void run(){
+				System.out.println("Frames: "+Frame.getFrames().length);
+				for (Frame frame: Frame.getFrames()) {
+					System.out.println(""+frame.getTitle());
+				}
+
+				for(long id: ManagementFactory.getThreadMXBean().getAllThreadIds()){
+					ThreadInfo ti = ManagementFactory.getThreadMXBean().getThreadInfo(id);
+					System.out.println("Thread: "+ti.getThreadName());
+				}
+			}
+		};
+		executor.scheduleAtFixedRate(helloRunnable, 0, 5, TimeUnit.SECONDS);
+	}
+
 }
