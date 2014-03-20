1f886c075c028f096d1f32670a8b648405cd51d4
Mikko Korpela <mikko.korpela@gmail.com>
2014-03-20 17:56:25 +0200

    no output

--- src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/TestJavaAgent.java
@@ -1,5 +1,7 @@
 package org.robotframework.rappio.agent;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
 import java.util.Properties;
@@ -17,29 +19,42 @@ import sun.awt.AppContext;
 
 public class TestJavaAgent {
 
+        private static PrintStream out;
+    
 	public static void premain(String agentArgument, Instrumentation instrumentation){
-                int port = Integer.parseInt(agentArgument);
-                Logger root = Logger.getRootLogger();
-                root.removeAllAppenders();
-                BasicConfigurator.configure();
-                root.setLevel(Level.OFF);
-                root.addAppender(new NullAppender());
-                LogFactory.releaseAll();
-                LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log",
-                        "org.apache.commons.logging.impl.Log4JLogger");
-                Properties p = new Properties();
-                p.setProperty("org.eclipse.jetty.LEVEL", "WARN");
-                org.eclipse.jetty.util.log.StdErrLog.setProperties(p);
-                RemoteServer server = new DaemonRemoteServer();
-                server.putLibrary("/RPC2", new SwingLibrary());
-                server.setPort(port);
-                server.setAllowStop(true);
-                try {
-                    server.start();
-                } catch (Exception e) {
-                    System.out.println("TODODOODODODODOODODODODO!!!");
-                }
-                AppContext.getAppContext();
+            noOutput();
+            int port = Integer.parseInt(agentArgument);
+            Logger root = Logger.getRootLogger();
+            root.removeAllAppenders();
+            BasicConfigurator.configure();
+            root.setLevel(Level.OFF);
+            root.addAppender(new NullAppender());
+            LogFactory.releaseAll();
+            LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log",
+                    "org.apache.commons.logging.impl.Log4JLogger");
+            Properties p = new Properties();
+            p.setProperty("org.eclipse.jetty.LEVEL", "WARN");
+            org.eclipse.jetty.util.log.StdErrLog.setProperties(p);
+            RemoteServer server = new DaemonRemoteServer();
+            server.putLibrary("/RPC2", new SwingLibrary());
+
+            server.setPort(port);
+            server.setAllowStop(true);
+            try {
+                server.start();
+            } catch (Exception e) {
+                System.out.println("TODODOODODODODOODODODODO!!!");
+            }
+            AppContext.getAppContext();
+            System.setOut(out);
 	}
 
+        private static void noOutput() {
+            out = System.out;
+            System.setOut(new PrintStream(new OutputStream() {
+                 public void write(int b) {
+                     //DO NOTHING
+                 }
+             }));
+        }
 }
