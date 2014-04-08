411b1bbb5d347a4f60ef669015056c953570a089
Mikko Korpela <mikko.korpela@gmail.com>
2014-04-08 12:50:18 +0300

    try this one

--- src/main/java/org/robotframework/rappio/agent/RappioJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/RappioJavaAgent.java
@@ -1,15 +1,64 @@
 package org.robotframework.rappio.agent;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 
 public class RappioJavaAgent {
 
 	private static final int DEFAULT_RAPPIO_PORT = 8181;
-
-	public static void premain(String agentArgument, Instrumentation instrumentation){
-		final int port = getRappioPort(agentArgument);      
-                Thread t = new Thread(new RappioServer(port));
-                t.start();
+        private static int PORT = DEFAULT_RAPPIO_PORT;
+        static Thread someThread = null;
+	static Instrumentation instrumentationInstance = null;
+	static ClassFileTransformer transformer = null;
+        
+        public static void premain(String agentArguments, final Instrumentation inst) throws Exception {
+            instrumentationInstance = inst;
+            PORT = getRappioPort(agentArguments);
+            if(isRunningJavaWebStart()) {
+                    transformer = new ClassListenerTransformer();
+                    inst.addTransformer(transformer);
+            } else {
+                    startRappioServer();
+            }
+        }
+        
+        public static void startRappioServer(ClassLoader classLoader) {
+            // Only do this once
+            if(someThread==null) {
+                @SuppressWarnings("rawtypes")
+                    Class runnableClass;
+                    try {
+                        instrumentationInstance.removeTransformer(transformer);
+                        runnableClass = classLoader.loadClass("org.robotframework.rappio.RappioServer");
+                        Object r = runnableClass.getDeclaredConstructor(Integer.class).newInstance(PORT);
+                        someThread = new Thread((Runnable) r);
+                        someThread.setContextClassLoader(classLoader);
+                        someThread.setDaemon(true);
+                        someThread.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }    	
+        }
+        
+        public static void startRappioServer() {
+            // Only do this once
+            if(someThread==null) {
+                    someThread = new Thread(new RappioServer(PORT));
+                    someThread.setDaemon(true);
+                    someThread.start();
+            }
+        }
+        
+        private static boolean isRunningJavaWebStart() {
+	    boolean hasJNLP = false;
+	    try {
+	      Class.forName("javax.jnlp.ServiceManager");
+	      hasJNLP = true;
+	    } catch (ClassNotFoundException ex) {
+	      hasJNLP = false;
+	    }
+	    return hasJNLP;
 	}
         
 	private static int getRappioPort(String agentArgument) {
