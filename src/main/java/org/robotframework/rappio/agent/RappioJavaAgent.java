255160c88c07ff65623f2cfa0d3825444d21475d
Mikko Korpela <mikko.korpela@gmail.com>
2014-04-09 13:41:13 +0300

    more code

--- src/main/java/org/robotframework/rappio/agent/RappioJavaAgent.java
+++ src/main/java/org/robotframework/rappio/agent/RappioJavaAgent.java
@@ -2,6 +2,7 @@ package org.robotframework.rappio.agent;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RappioJavaAgent {
@@ -15,14 +16,14 @@ public class RappioJavaAgent {
         public static void premain(String agentArguments, final Instrumentation inst) throws Exception {
             instrumentationInstance = inst;
             PORT = getRappioPort(agentArguments);
-            java.util.logging.Logger.getGlobal().info("OKSAT POIS");
             if(isRunningJavaWebStart()) {
-                    System.out.println("TANNE MENTIIN");
-                    transformer = new ClassListenerTransformer();
-                    inst.addTransformer(transformer);
-                    System.out.println("SIT ULOS");
+                Set<Thread> threads = Thread.getAllStackTraces().keySet();
+                for(Thread t:threads) {
+                    System.out.println("Thread:  "+t);
+                }
+                System.exit(0);
             } else {
-                    startRappioServer();
+                startRappioServer();
             }
         }
         
@@ -30,29 +31,29 @@ public class RappioJavaAgent {
             // Only do this once
             if(!STARTED.get()) {
                 @SuppressWarnings("rawtypes")
-                    Class runnableClass;
-                    try {
-                        instrumentationInstance.removeTransformer(transformer);
-                        runnableClass = classLoader.loadClass("org.robotframework.rappio.RappioServer");
-                        Object r = runnableClass.getDeclaredConstructor(Integer.class).newInstance(PORT);
-                        Thread someThread = new Thread((Runnable) r);
-                        someThread.setContextClassLoader(classLoader);
-                        someThread.setDaemon(true);
-                        someThread.start();
-                        STARTED.set(true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                Class runnableClass;
+                try {
+                    instrumentationInstance.removeTransformer(transformer);
+                    runnableClass = classLoader.loadClass("org.robotframework.rappio.RappioServer");
+                    Object r = runnableClass.getDeclaredConstructor(Integer.class).newInstance(PORT);
+                    Thread someThread = new Thread((Runnable) r);
+                    someThread.setContextClassLoader(classLoader);
+                    someThread.setDaemon(true);
+                    someThread.start();
+                    STARTED.set(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }    	
         }
         
         public static void startRappioServer() {
             // Only do this once
             if(!STARTED.get()) {
-                    Thread someThread = new Thread(new RappioServer(PORT));
-                    someThread.setDaemon(true);
-                    someThread.start();
-                    STARTED.set(true);
+                Thread someThread = new Thread(new RappioServer(PORT));
+                someThread.setDaemon(true);
+                someThread.start();
+                STARTED.set(true);
             }
         }
         
